a252818a353d38265a227559199c68ca6560f0ba
Maria Kilsved <Maria Kilsved>
2017-03-12 20:13:48 +0100

    2017-03-12

--- js/main.js
+++ js/main.js
@@ -50,10 +50,11 @@ var moviedatabase = (function () {
 
     /*Movie constructor*/
     /*Konstruktor för filmer*/
-    function Movie(title, year, genres) {
+    function Movie(title, year, genres, rating) {
         this.title = title;
         this.year = year;
         this.genres = genres;
+        this.rating = rating;
     };
 
     /*returns the average value of all ratings of a movie*/
@@ -288,6 +289,22 @@ var moviedatabase = (function () {
         return movie;
     };
 
+    /*add a new movie to the database*/
+    /*lägg till en ny film till databasen*/
+    var addMovie = () => {
+        /*Determine data*/
+        var title = document.getElementById('title').value;
+        var year = parseInt(document.getElementById('year').value);
+        var genres = determineGenres();
+        var rating = [parseInt(document.getElementById('rating').value)];
+
+        /*Create the new movie*/
+        var newMovie = new Movie(title, year, genres, rating);
+
+        /*Push into database*/
+        movies.push(newMovie);
+    };
+
     /*===PUBLIC===*/
     return {
         getPrintMovies: printMovies,
@@ -296,6 +313,315 @@ var moviedatabase = (function () {
         getprintMoviesByYear: printMoviesByYear,
         getTopRatedMovie: topRatedMovie,
         getWorstRatedMovie: worstRatedMovie,
-        getPrintPopularityInfo: printPopularityInfo
+        getPrintPopularityInfo: printPopularityInfo,
+        getAddMovie: addMovie
     }
+})();
+
+/*At first I used subpages, but then I discovered changes to the movies-array wouldn't translate to other pages. This is a fix.*/
+/*Till en början använde jag undersidor, men sedan upptäckte jag att ändringar i movies-array:en inte följde med till andra sidor. Det här är en fix.*/
+
+var subpages = (function () {
+    /*Collection of original elements*/
+    var printLi = document.getElementById('print-li');
+    var popularLi = document.getElementById('popular-li');
+    var genresLi = document.getElementById('genres-li');
+    var yearLi = document.getElementById('year-li');
+    var addMovieLi = document.getElementById('add-movie-li');
+    var content = document.getElementById('content');
+
+    /*function for setting multiple attributes at once*/
+    var setAttributes = (el, attrs) => {
+        for (var key in attrs) {
+            el.setAttribute(key, attrs[key]);
+        }
+    };
+
+    var makeCheckbox = (name, Name) => {
+        var check = document.createElement('div');
+        check.setAttribute("class", "check");
+        var input = document.createElement('input');
+        setAttributes(input, {
+            "type": "checkbox",
+            "value": "1",
+            "id": name,
+            "name": name
+        });
+        var label = document.createElement('label');
+        label.setAttribute("for", name);
+        var p = document.createElement('p');
+        p.innerHTML = Name;
+        p.setAttribute("class", "label");
+        check.appendChild(input);
+        check.appendChild(label);
+        check.appendChild(p);
+
+        return check;
+    };
+
+    /*===SUBPAGES===*/
+
+    /*load index*/
+    /*indexsidan*/
+    var index = () => {
+        /*clear content*/
+        content.innerHTML = '';
+
+        /*append HTML elements*/
+        var result = document.createElement('div');
+        result.setAttribute("id", "result");
+        content.appendChild(result);
+
+        /*function call to print all movies*/
+        moviedatabase.getPrintMovies();
+    };
+
+    /*load popularity page*/
+    /*popularitetssidan*/
+    var popular = () => {
+        /*clear content*/
+        content.innerHTML = '';
+
+        /*div for appending*/
+        var popularityInfo = document.createElement('div');
+        popularityInfo.id = "popularity-info";
+        content.appendChild(popularityInfo);
+
+        /*another div for appending*/
+        var result = document.createElement('div');
+        result.setAttribute("id", "result");
+        content.appendChild(result);
+
+        /*print data*/
+        moviedatabase.getPrintPopularityInfo();
+        moviedatabase.getPopularMovies();
+    };
+
+    /*load genres search*/
+    /*genre-sidan*/
+    var genres = () => {
+        /*clear content*/
+        content.innerHTML = '';
+
+        /*Heading*/
+        var heading = document.createElement('h3');
+        heading.innerHTML = 'Choose Genres';
+        document.body.appendChild(heading);
+
+        /*Positioning div*/
+        var div1 = document.createElement('div');
+        div1.setAttribute("class", "indentation");
+
+        /*Checkbox 1*/
+        var check1 = makeCheckbox("comedy", "Comedy");
+
+        /*Checkbox 2*/
+        var check2 = makeCheckbox("drama", "Drama");
+
+        /*Checkbox 3*/
+        var check3 = makeCheckbox("horror", "Horror");
+
+        /*Checkbox 4*/
+        var check4 = makeCheckbox("thriller", "Thriller");
+
+        /*append checkboxes*/
+        content.appendChild(check1);
+        content.appendChild(check2);
+        content.appendChild(check3);
+        content.appendChild(check4);
+
+        /*button for selection*/
+        var search = document.createElement('button');
+        search.innerHTML = "Search";
+        search.setAttribute("class", "indentation");
+        search.addEventListener('click', moviedatabase.getprintMoviesByGenre);
+
+        /*for appending*/
+        var result = document.createElement('div');
+        result.setAttribute("id", "result");
+        content.appendChild(result);
+    };
+
+    /*search by year*/
+    /*sökning efter år*/
+    var year = () => {
+        /*clear content*/
+        content.innerHTML = '';
+
+        /*creating and appending elements...*/
+        var div1 = document.createElement('div');
+        /*...heading*/
+        var heading = document.createElement('h3');
+        heading.innerHTML = "Choose a Year";
+        div1.appendChild(heading);
+        /*..year input*/
+        var input1 = document.createElement('input');
+        setAttributes(input1, {
+            "type": "number",
+            "id": "year",
+            "placeholder": "Year",
+            "max": "2100",
+            "min": "1890",
+            "step": 1
+        });
+        div1.appendChild(input1);
+        /*append*/
+        content.appendChild(div1);
+
+        /*button for selection*/
+        var button1 = document.createElement('button');
+        button1.innerHTML = "Search";
+        button1.setAttribute("class", "indentation");
+        button1.addEventListener('click', moviedatabase.getprintMoviesByYear);
+        content.appendChild(button1);
+
+        /*for appending*/
+        var result = document.createElement('div');
+        result.setAttribute("id", "result");
+        content.appendChild(result);
+    };
+
+    /*add a new movie*/
+    /*lägg till en ny film*/
+    var addMovies = () => {
+        /*clear content*/
+        content.innerHTML = '';
+
+        /*fieldset and legend*/
+        var fieldset = document.createElement('fieldset');
+        var legend = document.createElement('legend');
+        legend.innerHTML = "New Movie";
+        fieldset.appendChild(legend);
+
+        /*title input*/
+        var div1 = document.createElement('div');
+        var label1 = document.createElement('label');
+        label1.innerHTML = "Title";
+        label1.setAttribute("for", "title");
+        var input1 = document.createElement('input');
+        setAttributes(input1, {
+            "type": "text",
+            "name": "title",
+            "id": "title",
+            "placeholder": "Title"
+        });
+        input1.required = true;
+        div1.appendChild(label1);
+        div1.appendChild(input1);
+        fieldset.appendChild(div1);
+
+        /*year input*/
+        var div2 = document.createElement('div');
+        var label2 = document.createElement('label');
+        label2.innerHTML = "Premiere Year";
+        label2.setAttribute("for", "year");
+        var input2 = document.createElement('input');
+        setAttributes(input2, {
+            "type": "number",
+            "name": "year",
+            "id": "year",
+            "placeholder": "Year",
+            "max": "2100",
+            "min": "1890",
+            "step": "1"
+        });
+        div2.appendChild(label2);
+        div2.appendChild(input2);
+        fieldset.appendChild(div2);
+
+        /*checkboxes for genres*/
+        var check1 = makeCheckbox("comedy", "Comedy");
+        var check2 = makeCheckbox("drama", "Drama");
+        var check3 = makeCheckbox("horror", "Horror");
+        var check4 = makeCheckbox("thriller", "Thriller");
+
+        /*div around checkboxes*/
+        var div = document.createElement('div');
+
+        /*append checkboxes*/
+        div.appendChild(check1);
+        div.appendChild(check2);
+        div.appendChild(check3);
+        div.appendChild(check4);
+        fieldset.appendChild(div);
+
+        /*Select input...*/
+        var div3 = document.createElement('div');
+        div3.setAttribute("class", "select-positioner");
+
+        /*...label*/
+        var label3 = document.createElement('label');
+        label3.innerHTML = "Rating";
+        label3.setAttribute("for", "rating");
+
+        /*...style div*/
+        var div4 = document.createElement('div');
+        div4.setAttribute("class", "styled-select slate");
+
+        /*...actual select*/
+        var select = document.createElement('select');
+        setAttributes(select, {
+            "name": "rating",
+            "id": "rating"
+        });
+
+        /*...select options*/
+        var option1 = document.createElement('option');
+        option1.setAttribute("value", "1");
+        select.appendChild(option1);
+        var option2 = document.createElement('option');
+        option2.setAttribute("value", "2");
+        select.appendChild(option2);
+        var option3 = document.createElement('option');
+        option3.setAttribute("value", "3");
+        select.appendChild(option3);
+        var option4 = document.createElement('option');
+        option4.setAttribute("value", "4");
+        select.appendChild(option4);
+        var option5 = document.createElement('option');
+        option5.setAttribute("value", "5");
+        select.appendChild(option5);
+        var option6 = document.createElement('option');
+        option6.setAttribute("value", "6");
+        select.appendChild(option6);
+        var option7 = document.createElement('option');
+        option7.setAttribute("value", "7");
+        select.appendChild(option7);
+        var option8 = document.createElement('option');
+        option8.setAttribute("value", "8");
+        select.appendChild(option8);
+        var option9 = document.createElement('option');
+        option9.setAttribute("value", "9");
+        select.appendChild(option9);
+        var option10 = document.createElement('option');
+        option10.setAttribute("value", "10");
+        select.appendChild(option10);
+
+        /*..append together*/
+        div4.appendChild(select);
+        div3.appendChild(label3);
+        div3.appendChild(div4);
+        fieldset.appendChild(div3);
+
+        /*Button*/
+        var submit = document.createElement('button');
+        submit.innerHTML = "Submit";
+        setAttributes(submit, {
+            "name": "submit",
+            "id": "submit"
+        });
+        submit.addEventListener('click', moviedatabase.getAddMovie);
+        fieldset.appendChild(submit);
+
+        /*final append*/
+        content.appendChild(fieldset);
+    };
+
+    /*Adding event listeners*/
+    printLi.addEventListener('click', index);
+    popularLi.addEventListener('click', popular);
+    genresLi.addEventListener('click', genres);
+    yearLi.addEventListener('click', year);
+    addMovieLi.addEventListener('click', addMovies);
+
 })();
